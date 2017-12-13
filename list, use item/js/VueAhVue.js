4ebf23c656ceb0ae780d4516e7cb22757e068cec
Dasha <etterwin.yll@gmail.com>
2017-12-13 12:03:49 +0300

    upload

---
+++ list, use item/js/VueAhVue.js
@@ -0,0 +1,52 @@
+new Vue ({
+    el: "#app",
+    data: {
+        bears: [
+            {
+                name:'Ursus arctos',
+                status: false
+            }, {
+                name:'Ursus americanus',
+                status: true
+            }, {
+                name:'Ursus maritimus',
+                status: false
+            }, {
+                name:'Ursus thibetanus',
+                status: true
+            }, {
+                name:'Ursus thibetanus',
+                status: false
+            }, {
+                name:'Tremarctos',
+                status: true
+            }, {
+                name:'Arctodus',
+                status: false
+            }, {
+                name:'Ursus etruscus',
+                status: true
+            }, {
+                name:'Ursus spelaeus',
+                status: false
+            }, {
+                name:'Ursus rossicus',
+                status: true
+            }, {
+                name:'Ursus minimus',
+                status: false
+            },
+        ]
+
+    }
+});
+
+
+
+
+
+
+
+
+
+
