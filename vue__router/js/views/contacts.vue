7add6487b4fa2304bc4b8a50a1aa31f73c269a2d
etterwin <etterwin.yll@gmail.com>
2018-03-20 16:51:30 +0300

    update

---
+++ vue__router/js/views/contacts.vue
@@ -0,0 +1,5 @@
+<template>
+    <section class="container">
+        <h2>Contacts</h2>
+    </section>
+</template>
---
+++ vue__router/js/views/projects.vue
@@ -0,0 +1,5 @@
+<template>
+    <section class="container">
+        <h2>Projects</h2>
+    </section>
+</template>
